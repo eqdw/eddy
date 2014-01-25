df885ee39c75984d83f1d2d8d5963bfdbdbb4c50
James Bobowski <james@fandor.com>
2014-01-25 15:21:35 -0800

    Examples, skeleton code for the Eddy gem to make menus with submenus

---
+++ lib/eddy/menu.rb
@@ -0,0 +1,11 @@
+module Eddy
+  class Menu
+    def initialize(options = {})
+      # TODO Tim Parse options into Menu class
+    end
+    
+    def execute
+      # TODO Tim Display border with appropriate options displayed
+    end
+  end
+end
---
+++ lib/eddy/program.rb
@@ -0,0 +1,14 @@
+module Eddy
+  class Program
+    def initialize
+      @main_menu = nil
+    end
+    
+    def main_menu
+    end
+    
+    def launch
+      @main_menu.execute
+    end
+  end
+end
