877a803525ed22a82f8e4bebbf8f2ba4289f86c8
Tim Herd <rz@eqdw.net>
2014-01-25 16:20:45 -0800

    Centered title

--- lib/eddy/menu.rb
+++ lib/eddy/menu.rb
@@ -2,18 +2,31 @@ module Eddy
   class Menu
     def initialize(options = {})
       @title = options[:title]
-      @items = options[:items]      
+      @items = options[:items]
     end
-    
+
     def render
-      scr = ::Ncurses.stdscr
-      scr.clear()
-      scr.border(*([0]*8)) # calls WINDOW#border(0, 0, 0, 0, 0, 0, 0, 0)
-      scr.move(3,3)
-      scr.addstr("Press a key to continue")
-      scr.getch()
-      
+      @scr = ::Ncurses.stdscr
+      @scr.clear()
+      @scr.border(*([0]*8)) # calls WINDOW#border(0, 0, 0, 0, 0, 0, 0, 0)
+
+      print_centered_title
+
+
+      @scr.move(3,3)
+      @scr.addstr("Press a key to continue")
+      @scr.getch()
+
       # TODO Tim Display border with appropriate options displayed
+    end
+
+    def print_centered_title
+      title_offset = @title.length / 2
+      midpoint     = ::Ncurses.COLS / 2
+
+      @scr.move(0, midpoint - title_offset)
+      @scr.addstr(@title)
+
     end
   end
 end
